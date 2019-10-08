d8b775f2f52ec4bbf475f1543c0f5aa0ff7ea539
Manuel J Saavedra Severiche <saavedramanuel100@gmail.com>
2019-10-07 23:13:49 -0400

    Implementacion del Login (Modal PopUp) - 90% Terminado

--- wwwroot/js/contact.js
+++ wwwroot/js/contact.js
@@ -27,127 +27,128 @@ jQuery(document).ready(function ($) {
             default:
                 break;
         }
-});
+    });
 
 
-function showContainerTransporteNacional() {
-    $("#divPesoVolumen").hide();
-    $("#divAlmacenamientoCarga").hide();
-    $("#divTransporteNacional").show();
-}
-
-
-function showContainerTransporteUrbanoSCZ() {
-    $("#divTransporteNacional").hide();
-    $("#divAlmacenamientoCarga").hide();
-    $("#divPesoVolumen").show();
-}
-
-function showContainerAlmacenamientoCarga() {
-    $("#divTransporteNacional").hide();
-    $("#divPesoVolumen").hide();
-    $("#divAlmacenamientoCarga").show();
-}
-
-function initFormCarrier() {
-
-    /*changeStateSelectorByName("loadingSource", false);
-    changeStateSelectorByName("loadingDestination", false);*/
-
-}
-
-function initFormCustomer() {
-
-}
-
-
-function changeStateLabelForSelectorById(id, enable = true) {
-    var colorBack = enable ? "#000000" : "#e6e6e5";
-    $(id).css("color", colorBack);    
-}
-   
-
-function changeStateSelectorByName(name, enable = true) {
-    if (enable) { // habilitar 
-        $("select[name=" + name + "]")
-            .css("background-color", "#FFFF")
-            .css("color", "#000000")
-            .removeAttr("disabled");
-    } else { // deshabilitar
-        $("select[name=" + name + "]")
-            .css("background-color", "#e6e6e5")
-            .css("color", "#e6e6e5")
-            .attr("disabled",true);
+    function showContainerTransporteNacional() {
+        $("#divPesoVolumen").hide();
+        $("#divAlmacenamientoCarga").hide();
+        $("#divTransporteNacional").show();
     }
-    
-}
-
-function responseCotization() {
-
-    var $formContact = $('form')[0];
-    if ($formContact.checkValidity()) {
-        var contactRequest = {
-            company: $('input[name=company]').val(),
-            fullname: $('input[name=fullname]').val(),
-            phone: $('input[name=phone]').val(),
-            email: $('input[name=email]').val(),
-            serviceType: $('select[name=serviceType]').val(),
-            transportRoute: {
-                loadingSource: $('select[name=loadingSource]').val(),
-                loadingDestination: $('select[name=loadingDestination]').val()
-            }
-        };
-        $.post("/Contact/ContactForm/",
-            contactRequest,
-            function (data, status, jqXHR) {
-                Swal.fire({
-                    title: 'Solicitud de Cotizacion',
-                    text: data.message,
-                    type: data.code == 200 ? 'success' : 'error',
-                    showConfirmButton: false,
-                    timer: 2000
-                });
-                clearFields();
-            }, "json")
-            .always(function (data) {
-                Swal.fire({
-                    title: 'Solicitud de Cotizacion',
-                    text: data.message,
-                    type: data.code == 200 ? 'success' : 'error',
-                    showConfirmButton: false,
-                    timer: 2000
-                });
-                clearFields();
-            })
-            .fail(function (error) { });
-        return false;    
-    } 
-    return true;
-    
-    /*if (contactRequest.fullname != '' && contactRequest.phone != '' && contactRequest.serviceType != 0) {
-        
-    }*/
-}
-
-function clearFields() {
-    $('input[name=company]').val('');
-    $('input[name=fullname]').val('');
-    $('input[name=phone]').val('');
-    $('input[name=email]').val('');
-}
 
 
-function onlyAlert() {
-    setTimeout(function () {
-        Swal.fire({
-            title: 'Solicitud de cotizacion enviada!!!',
-            text: 'Do you want to continue',
-            type: 'success',
-            confirmButtonText: 'OK'
-        });
-    }, 1000);
-    return false;
-}
+    function showContainerTransporteUrbanoSCZ() {
+        $("#divTransporteNacional").hide();
+        $("#divAlmacenamientoCarga").hide();
+        $("#divPesoVolumen").show();
+    }
+
+    function showContainerAlmacenamientoCarga() {
+        $("#divTransporteNacional").hide();
+        $("#divPesoVolumen").hide();
+        $("#divAlmacenamientoCarga").show();
+    }
+
+    function initFormCarrier() {
+
+        /*changeStateSelectorByName("loadingSource", false);
+        changeStateSelectorByName("loadingDestination", false);*/
+
+    }
+
+    function initFormCustomer() {
+
+    }
+
+
+    function changeStateLabelForSelectorById(id, enable = true) {
+        var colorBack = enable ? "#000000" : "#e6e6e5";
+        $(id).css("color", colorBack);
+    }
+
+
+    function changeStateSelectorByName(name, enable = true) {
+        if (enable) { // habilitar 
+            $("select[name=" + name + "]")
+                .css("background-color", "#FFFF")
+                .css("color", "#000000")
+                .removeAttr("disabled");
+        } else { // deshabilitar
+            $("select[name=" + name + "]")
+                .css("background-color", "#e6e6e5")
+                .css("color", "#e6e6e5")
+                .attr("disabled", true);
+        }
+
+    }
+
+    function responseCotization() {
+
+        var $formContact = $('form')[0];
+        if ($formContact.checkValidity()) {
+            var contactRequest = {
+                company: $('input[name=company]').val(),
+                fullname: $('input[name=fullname]').val(),
+                phone: $('input[name=phone]').val(),
+                email: $('input[name=email]').val(),
+                serviceType: $('select[name=serviceType]').val(),
+                transportRoute: {
+                    loadingSource: $('select[name=loadingSource]').val(),
+                    loadingDestination: $('select[name=loadingDestination]').val()
+                }
+            };
+            $.post("/Contact/ContactForm/",
+                contactRequest,
+                function (data, status, jqXHR) {
+                    Swal.fire({
+                        title: 'Solicitud de Cotizacion',
+                        text: data.message,
+                        type: data.code == 200 ? 'success' : 'error',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                    clearFields();
+                }, "json")
+                .always(function (data) {
+                    Swal.fire({
+                        title: 'Solicitud de Cotizacion',
+                        text: data.message,
+                        type: data.code == 200 ? 'success' : 'error',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                    clearFields();
+                })
+                .fail(function (error) { });
+            return false;
+        }
+        return true;
+
+        /*if (contactRequest.fullname != '' && contactRequest.phone != '' && contactRequest.serviceType != 0) {
+            
+        }*/
+    }
+
+    function clearFields() {
+        $('input[name=company]').val('');
+        $('input[name=fullname]').val('');
+        $('input[name=phone]').val('');
+        $('input[name=email]').val('');
+    }
+
+
+    function onlyAlert() {
+        setTimeout(function () {
+            Swal.fire({
+                title: 'Solicitud de cotizacion enviada!!!',
+                text: 'Do you want to continue',
+                type: 'success',
+                confirmButtonText: 'OK'
+            });
+        }, 1000);
+        return false;
+    }
+});
 
 
 
